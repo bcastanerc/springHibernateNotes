e57b5e551c5cbb2af003b2f19d10082ea3c70f07
bcastanerc <60187468+bcastanerc@users.noreply.github.com>
2021-02-02 14:05:38 +0100

    Creado registro y login, implementados interceptors de loging y csrf.

--- src/main/java/com/liceu/demoHibernate/config/AppConfig.java
+++ src/main/java/com/liceu/demoHibernate/config/AppConfig.java
@@ -1,17 +1,43 @@
 package com.liceu.demoHibernate.config;
 
+import com.liceu.demoHibernate.interceptors.CheckCsrfToken;
+import com.liceu.demoHibernate.interceptors.GenerateCsrfToken;
+import com.liceu.demoHibernate.interceptors.SessionInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.liceu.demoHibernate")
 public class AppConfig implements WebMvcConfigurer {
+
+    @Autowired
+    SessionInterceptor sessionInterceptor;
+
+    @Autowired
+    GenerateCsrfToken generateCsrfToken;
+
+    @Autowired
+    CheckCsrfToken checkCsrfToken;
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(sessionInterceptor).addPathPatterns(getInterceptorMapping());
+        registry.addInterceptor(generateCsrfToken).addPathPatterns("/*");
+        registry.addInterceptor(checkCsrfToken).addPathPatterns("/*");
+    }
 
     @Bean
     public UrlBasedViewResolver viewResolver() {
@@ -21,5 +47,17 @@ public class AppConfig implements WebMvcConfigurer {
         resolver.setViewClass(JstlView.class);
         return resolver;
     }
+
+    @Bean
+    public List<String> getInterceptorMapping(){
+        List<String> interceptorMapingList = new ArrayList<>();
+        interceptorMapingList.add("/viewNote");
+        interceptorMapingList.add("/updateNote");
+        interceptorMapingList.add("/userNotes");
+        interceptorMapingList.add("/userInfo");
+        interceptorMapingList.add("/createNotes");
+        return interceptorMapingList;
+    }
+
 
 }
--- src/main/java/com/liceu/demoHibernate/controllers/Controller1.java
+++ src/main/java/com/liceu/demoHibernate/controllers/Controller1.java
@@ -1,13 +1,24 @@
 package com.liceu.demoHibernate.controllers;
 
+import com.liceu.demoHibernate.entities.Note;
+import com.liceu.demoHibernate.entities.User;
+import com.liceu.demoHibernate.services.NoteService;
+import com.liceu.demoHibernate.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -15,10 +26,71 @@ import java.util.List;
 @Controller
 public class Controller1 {
 
-    @GetMapping("/hola")
-    @ResponseBody
-    public String getPersons(){
-        return "hola";
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    NoteService noteService;
+
+    @GetMapping("/register")
+    public String getRegister(){
+        return "/register";
     }
+
+    @PostMapping("/register")
+    public String registerUser(Model model, @RequestParam String _csrftoken, @RequestParam String email, @RequestParam String password, @RequestParam String username, @RequestParam String confirmPassword) throws NoSuchAlgorithmException, IOException {
+        if (password.equals(confirmPassword) && userService.isPasswordValid(password) && userService.isEmailValid(email) &&  userService.isUsernameValid(username)){
+            User u = new User();
+            u.setUsername(username);
+            u.setEmail(email);
+            u.setPassword(userService.encryptPassword(password));
+            userService.save(u);
+            return "redirect:/login";
+        }else{
+            model.addAttribute("error", true);
+            model.addAttribute("csrfToken", _csrftoken);
+            return "/register";
+        }
+    }
+
+    @GetMapping("/login")
+    public String getLogin(){
+        return "/login";
+    }
+
+    @PostMapping("/login")
+    public String postLogin(@RequestParam String email, @RequestParam String password, HttpSession session, Model model,@RequestParam String _csrftoken) throws NoSuchAlgorithmException {
+        User logedUSer = userService.findUserByEmailEquals(email);
+        String encryptedPassword = userService.encryptPassword(password);
+        if (logedUSer != null && logedUSer.getPassword().equals(encryptedPassword)){
+            session.setAttribute("user_id", logedUSer.getId());
+            return "redirect:/createNotes";
+        }else{
+            model.addAttribute("error", true);
+            model.addAttribute("csrfToken", _csrftoken);
+            return "/login";
+        }
+    }
+
+    @GetMapping("/createNotes")
+    public String getCreateNotes(){
+        return "/createNotes";
+    }
+
+    @GetMapping("/saveNote")
+    @ResponseBody
+    public String saveNote(){
+       Note n = new Note();
+       n.setUser(userService.findById(1L));
+       n.setTitle("Prueba nota");
+       n.setText("Hola que tal");
+        return noteService.save(n).toString();
+    }
+
+    @GetMapping("/error")
+    public String getError(){
+        return "/error";
+    }
+
 
 }
